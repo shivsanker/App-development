6e477cef6ca3f75e3a2f79f91ca03d5d94f7b9a6
shivsanker <120246391+shivsanker@users.noreply.github.com>
2023-10-16 12:15:50 +0530

    Add files via upload

---
+++ repo day 20/shiv/src/main/java/com/neptune/feedback/controller/FeedbackController.java
@@ -0,0 +1,41 @@
+package com.neptune.feedback.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.neptune.feedback.dto.request.FeedbackRequest;
+import com.neptune.feedback.constant.Api;
+import com.neptune.feedback.dto.response.FeedbackResponse;
+import com.neptune.feedback.service.FeedbackService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping(Api.FEEDBACK)
+@RequiredArgsConstructor
+public class FeedbackController {
+
+	private final FeedbackService feedbackService;
+	
+	@PostMapping("/add")
+    public ResponseEntity<String> saveFeedback(@RequestBody FeedbackRequest request) {
+        boolean isSaved = feedbackService.saveFeedback(request);
+        return isSaved ? ResponseEntity.ok().body("Feedback Saved") : ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<List<FeedbackResponse>> getFeedbacks() {
+        List<FeedbackResponse> feedbackList = feedbackService.getFeedbacks();
+        return !feedbackList.isEmpty() ? ResponseEntity.ok(feedbackList) : ResponseEntity.noContent().build();
+    }
+
+}
